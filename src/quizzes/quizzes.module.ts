78e937ddfb9a122e7e55cd3f303443bdeb707382
Sirio <sirioroberto1@hotmail.com>
2023-12-15 20:48:55 +0000

    Removed unecessary imports from Quiz module

--- src/quizzes/quizzes.module.ts
+++ src/quizzes/quizzes.module.ts
@@ -3,15 +3,9 @@ import { QuizzesService } from './quizzes.service';
 import { QuizzesController } from './quizzes.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Quiz } from './entities/quiz.entity';
-import { AuthService } from 'src/auth/auth.service';
-import { HttpStrategy } from 'src/auth/http-strategy.auth';
-import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Quiz]),
-    PassportModule.register({ defaultStrategy: 'basic' }),
-  ],
+  imports: [TypeOrmModule.forFeature([Quiz])],
   controllers: [QuizzesController],
   providers: [QuizzesService],
 })
